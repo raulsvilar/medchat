29bdbab12e6436d98feddb1fa583c0800cea3881
raulsvilar <raul.s.vilar@gmail.com>
2020-03-18 21:28:12 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/dev/raulsvilar/medchat/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.raulsvilar.medchat
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
